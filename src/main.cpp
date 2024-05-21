e0ded37239c73b33cfa96e26a11701a1fc172df5
Martinson50 <martin.rusia@gmail.com>
2024-05-21 11:35:46 +0200

    Traducciendo as español

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void setup() {
   // put your setup code here, to run once:
     Serial.begin(115200);
     elog.addSerialLogging(Serial, "Main", DEBUG);
-    elog.log(ALERT, "Sketch started");
+    elog.log(ALERT, "Progtama arrancado");
     int result = myFunction(2, 3);
 }
 
@@ -18,11 +18,11 @@ void loop() {
   // put your main code here, to run repeatedly:
 
       for (uint16_t counter = 0; counter < 1000; counter++) {
-        elog.log(INFO, "Our counter is %d", counter);
+        elog.log(INFO, "Nuestro contador es %d", counter);
         if (counter == 3) { // Normally we dont have time from the very beginning. This simulates behaveour
             // We come up with 15th of july 2023 at 08:12:34
             // But find the time in your own way. There are many
-            elog.log(NOTICE, "Now we provide some real human time");
+            elog.log(NOTICE, "Ahora damos el tiempo real");
             Elog::provideTime(2023, 7, 15, 8, 12, 34);
         }
         delay(1007);
